dfe90d88f44c51d573e125050abde31e54a87172
Skylark96 <skylark.b96@gmail.com>
2023-03-25 15:10:52 +0100

    finished

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard'
 
 const MovieList = ({data}) => {
   return (
-    <div className='main'>
+    <div className='list-box'>
         {
         React.Children.toArray( data.map((movie)=><MovieCard xmovie={movie}/>))
         }
--- src/Components/StarsRating.js
+++ src/Components/StarsRating.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import {FaStar} from "react-icons/fa"
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
-const StarsRating = ({rating}) => {
+const StarsRating = ({ rating,handleRate }) => {
+    const [Hover, setHover] = useState(null)
   return (
     <div>
-        {[...Array(5)].map((star,i)=>{
-            const stars=i+1
-        return <label>
-            <input type="radio" name="stars" value={rating}/>
-            <FaStar id='rating' 
-            color={stars <= rating ? "#ffc107" : "#e4e5e9"}/>
-        </label>
-        })}
+      {[...Array(5)].map((star, i) => {
+        const stars = i + 1;
+        return (
+          <label>
+            <input type="radio" name="stars" value={rating} onClick={()=>handleRate(stars)}/>
+            <FaStar
+              id="rating"
+              color={stars <= (Hover || rating) ? "#ffc107" : "#e4e5e9"}
+              onMouseEnter={()=>setHover(stars)}
+              onMouseLeave={()=>setHover(null)}
+            />
+          </label>
+        );
+      })}
     </div>
-  )
+  );
+};
+StarsRating.defaultProps={
+  handleRate:()=>{}
 }
-
-export default StarsRating
+export default StarsRating;
